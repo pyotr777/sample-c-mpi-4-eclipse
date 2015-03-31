83c06881e7ed9ddd402c1c93cfdba182801e584a
Peter Bryzgalov <peterbryz@yahoo.com>
2015-03-31 14:23:47 +0900

    Script monitor_para.pl for monitoring running processes.

---
+++ monitor_para.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @pids;
+my @procs;
+my $print=0;
+my @words;
+@pids=`ps ax | grep "monitor_para" | awk '{ print \$1 }'`;
+print @pids;
+while (1) {
+	@procs=`ps ax | egrep -i 'paraprof|c_mpi' | grep -v "c_mpi.o" | grep -v "grep"`;
+#	@procs=`ps ax | egrep -i '_para|c_mpi'`;
+	for my $proc (@procs) {
+		$print=1;
+		chomp($proc);
+		for my $pid (@pids) {
+			chomp($pid);
+			if ($proc =~ /^$pid /) {
+				$print=0;
+				last;
+			}
+		}
+		if ($print==1) {
+			@words = split(/ /,$proc);
+			print "\n\$ $proc";
+			push @pids, $words[0];
+		}
+	}
+}
